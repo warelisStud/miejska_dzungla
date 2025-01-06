5db63cc65f7f2e45135876139c07541444075f94
wwarir <lukasz.witczyk@edu.uekat.pl>
2025-01-06 12:27:12 +0100

    6.5

--- frontend/miejska_dzungla (w java)/src/main/java/com/example/miejska_dzungla/category/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.miejska_dzungla.category;
-
-import org.hibernate.type.descriptor.converter.spi.JpaAttributeConverter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- frontend/miejska_dzungla (w java)/src/main/java/com/example/miejska_dzungla/order/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.miejska_dzungla.order;
-
-import jakarta.persistence.Id;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
